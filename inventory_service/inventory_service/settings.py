3f56b0729072ba2a6d347ec2bc7c0ffa92bbf8bc
Muhammad Adnan <adnasir607@gmail.com>
2024-08-09 20:58:41 +0500

    envs in setting

--- inventory_service/inventory_service/settings.py
+++ inventory_service/inventory_service/settings.py
@@ -6,12 +6,17 @@ try:
 except FileNotFoundError:
     config = Config()
 DATABASE_URL="postgresql://ziakhan:my_password@postgres_db:5432/mydatabase"
-# DATABASE_URL = config("postgresql://ziakhan:my_password@postgres_db:5432/mydatabase", cast=Secret)
-BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
-KAFKA_ORDER_TOPIC = config("KAFKA_ORDER_TOPIC", cast=str)
-KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT = config("KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT", cast=str)
+TEST_DATABASE_URL="postgresql://ziaukhan:oSUELz91i@ep-polished-waterfall-a50jz332.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
-TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
+BOOTSTRAP_SERVER = "broker:19092"
+KAFKA_ORDER_TOPIC = "inventory"
+KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT = "consumer-group-id"
+# DATABASE_URL = config("postgresql://ziakhan:my_password@postgres_db:5432/mydatabase", cast=Secret)
+# BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
+# KAFKA_ORDER_TOPIC = config("KAFKA_ORDER_TOPIC", cast=str)
+# KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT = config("KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT", cast=str)
+
+# TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
 
 ALGORITHM = "HS256"
 SECRET_KEY = "A Secure Secret Key"
