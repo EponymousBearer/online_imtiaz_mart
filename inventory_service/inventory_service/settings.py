4a7b475131d46034c4b0e9de66e06f4a76faa4f6
Muhammad Adnan <adnasir607@gmail.com>
2024-08-09 20:53:28 +0500

    one more try

--- inventory_service/inventory_service/settings.py
+++ inventory_service/inventory_service/settings.py
@@ -5,8 +5,8 @@ try:
     config = Config(".env")
 except FileNotFoundError:
     config = Config()
-
-DATABASE_URL = config("postgresql://ziakhan:my_password@postgres_db:5432/mydatabase", cast=Secret)
+DATABASE_URL="postgresql://ziakhan:my_password@postgres_db:5432/mydatabase"
+# DATABASE_URL = config("postgresql://ziakhan:my_password@postgres_db:5432/mydatabase", cast=Secret)
 BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
 KAFKA_ORDER_TOPIC = config("KAFKA_ORDER_TOPIC", cast=str)
 KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT = config("KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT", cast=str)
